1cabe167941c47857d364cf04bc5193a28355620
sanwal-khan <sialsanwal885@gmail.com>
2025-07-03 17:07:42 +0500

    adding the file

--- Document_Loaders/text_loader.py
+++ Document_Loaders/text_loader.py
@@ -1,6 +1,13 @@
 from langchain_community.document_loaders import TextLoader
 
-loader= TextLoader('../data/poem.txt')
+loader = TextLoader('../data/poem.txt')
 
-docs=loader.load()
-print(docs)
+docs = loader.load()
+
+print(type(docs)) # its show the type of docs is list.
+print("_______________")
+
+print(docs[0].metadata)
+print("_______________")
+print(docs[0].page_content)
+
