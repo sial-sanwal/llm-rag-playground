693ca89ed95808395dc4974a36a2324e5d9a51d2
sanwal-khan <sialsanwal885@gmail.com>
2025-07-03 18:42:39 +0500

    Adding the pdf loader

--- Document_Loaders/pdf_loader.py
+++ Document_Loaders/pdf_loader.py
@@ -1,3 +1,12 @@
+# | **Use Case**                   | **Recommended Loader**                               |
+# | ------------------------------ | ---------------------------------------------------- |
+# | Simple, clean PDFs             | `PyPDFLoader`                                        |
+# | PDFs with tables/columns       | `PDFPlumberLoader`                                   |
+# | Scanned/image PDFs             | `UnstructuredPDFLoader` or `AmazonTextractPDFLoader` |
+# | Need layout and image data     | `PyMuPDFLoader`                                      |
+# | Want best structure extraction | `UnstructuredPDFLoader`                              |
+
+
 from langchain_community.document_loaders import PyPDFLoader
 
 loader= PyPDFLoader("../data/document.pdf")
